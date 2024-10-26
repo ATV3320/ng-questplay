2c8b47ebefd38eab992d49bc5bbfe8e6b7086591
Someone <someonenosurname@gmail.com>
2024-10-26 15:47:53 +0530

    #solitidy-optimization

--- campaigns/gas-optimization/solidity-optimization/contracts/Challenge.sol
+++ campaigns/gas-optimization/solidity-optimization/contracts/Challenge.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.19;
 
 contract Challenge {
 
-    uint256 _SKIP;
+    uint256 immutable _SKIP;
 
     constructor(uint256 skip) {
         _SKIP = skip;
@@ -14,12 +14,21 @@ contract Challenge {
      * @param array The array to sum.
      * @return sum The sum of all the elements of the array excluding SKIP.
      */
-    function sumAllExceptSkip(
-        uint256[] calldata array
-    ) public view returns (uint256 sum) {
+    function sumAllExceptSkip(uint256[] calldata array) public view returns (uint256 sum) {
+        uint256 skip = _SKIP;
 
-        // IMPLEMENT HERE
-        
+        assembly {
+            let end := add(array.offset, mul(array.length, 0x20)) // End pointer of array in calldata
+            let ptr := array.offset // Starting pointer of array in calldata
+
+            for { } lt(ptr, end) { ptr := add(ptr, 0x20) } {
+                let val := calldataload(ptr) // Loading current array element
+                if eq(val, skip) { continue } //skip
+
+                // Perform unchecked addition and update sum
+                sum := add(sum, val)
+                if lt(sum, val) { revert(0, 0) } // Revert on overflow
+            }
+        }
     }
-
 }
